e8e47d9a7306cd159fa7fb8513599eb9596a0ec7
Pablo Díaz Ogni <pablo.diazogni@gmail.com>
2012-11-15 17:57:37 -0300

    Agregue Entry para setear el path

--- explorer.py
+++ explorer.py
@@ -33,10 +33,10 @@ def get_files(path):
             return None
         return fname
 
-
 def get_music(path):
     doc = {}
-    data = _get_music_files(path)
+    data = []
+    _get_music_files(path, data)
 
     for value in data:
         artist = value['artist']
@@ -51,18 +51,12 @@ def get_music(path):
             doc[artist][album].append((title, fpath))
     return doc
 
-def _get_music_files(path, data=[]):
+def _get_music_files(path, data):
     path = os.path.abspath(path)
     if os.path.isdir(path):
         for f in os.listdir(path):
             new_path = path + '/' + f
-            value = _get_music_files(new_path)
-            if value is not None:
-                if isinstance(value, list):
-                    data = value
-                else:
-                    data.append(value)
-        return data
+            _get_music_files(new_path, data)
     else:
         ext = os.path.splitext(path)
         if ext[1] not in ['.mp3']:
@@ -77,9 +71,9 @@ def _get_music_files(path, data=[]):
                 doc['album'] = mfile['album'].pop()
             if 'title' in mfile:
                 doc['title'] = mfile['title'].pop()
+            data.append(doc)
         except:
-            return None
-        return doc
+            pass
 
 
 if __name__ == '__main__':
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import os
+
 import pygtk
 pygtk.require('2.0')
 import gtk
@@ -37,13 +39,20 @@ class MetaFixer(gtk.Window):
         box.pack_end(self.quit_button, expand=False)
         box.show()
         self.buttons_box = box
+
+        # Path text
+        path_text = AutocompleteEntry(tree.load)
         
         # Main box
-        box = gtk.HBox()
-        box.pack_start(tree)
-        box.pack_end(self.buttons_box, expand=False, fill=False)
-        box.show()
-        self.main_box = box
+        box2 = gtk.VBox(spacing=5)
+        box2.pack_start(tree)
+        box2.pack_end(path_text, expand=False)
+        box2.show()
+        box1 = gtk.HBox(spacing=10)
+        box1.pack_start(box2)
+        box1.pack_end(self.buttons_box, expand=False, fill=False)
+        box1.show()
+        self.main_box = box1
         self.add(self.main_box)
 
         self.set_size_request(300, 400)
@@ -56,21 +65,26 @@ class MetaFixer(gtk.Window):
         gtk.main_quit()
 
 
-class MusicFilesTreeView(gtk.TreeView):
+class MusicFilesTreeView(gtk.ScrolledWindow):
 
     def __init__(self):
         super(MusicFilesTreeView, self).__init__()
         self.path = '/home/qa/Music'
         self.load(self.path)
+        self.add(self.treeview)
 
     def load(self, path):
+        self.files = []
         self.path = path
+
         self.data = explorer.get_music(self.path)
         self.treestore = gtk.TreeStore(str)
         self._load_treestore(self.data)
-        super(MusicFilesTreeView, self).__init__(self.treestore)
+        self.treeview = gtk.TreeView(self.treestore)
+        self.treeview.set_reorderable(True)
+
         self.tree_view_column = gtk.TreeViewColumn('Column')
-        self.append_column(self.tree_view_column)
+        self.treeview.append_column(self.tree_view_column)
         self.cell_renderer = gtk.CellRendererText()
         self.tree_view_column.pack_start(self.cell_renderer, True)
         self.tree_view_column.add_attribute(self.cell_renderer, 'text', 0)
@@ -83,9 +97,28 @@ class MusicFilesTreeView(gtk.TreeView):
                self._load_treestore(value, new_parent)
         elif isinstance(data, list):
             for value in data:
-                self.treestore.append(parent, [value[0]])
+                i = self.treestore.append(parent, [value[0]])
+                self.files.append((i, value[1]))
         else:
             self.treestore.append(parent, [data])
+
+
+class AutocompleteEntry(gtk.Entry):
+
+    def __init__(self, enter_callback):
+        super(AutocompleteEntry, self).__init__()
+        self.connect('key_press_event', self.key_press)
+        self.path = os.getcwd()
+        self.set_text(self.path)
+        self.enter_callback = enter_callback
+        
+
+    def key_press(self, widget, key):
+        if key.keyval in (65289, 65056):
+            return True
+        if key.keyval == 65293:
+            self.enter_callback(self.get_text())
+        return False
 
 
 if __name__ == '__main__':
